569a94e58120b7d02e2ad26b7ae6ae63de1e971f
thiago <nasciutti.thiago@gmail.com>
2022-09-07 19:34:44 -0400

    bootstrap

--- index.js
+++ index.js
@@ -1,73 +1,159 @@
 const inquirer = require("inquirer");
-const Employee = require("./lib/Employee");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const buildHtml = require("./src/template");
+const teamMembers = [];
 
 //Manager Questions
 const managerQuestions = [
-  {
-    type: "input",
-    name: "managerName",
-    message: "What is the Manager's Name ?",
-  },
+ {
+  type: "input",
+  name: "managerName",
+  message: "What is the Manager's Name ?",
+ },
+ {
+  type: "input",
+  name: "managerId",
+  message: "What is the Manager's Id ?",
+ },
+ {
+  type: "input",
+  name: "managerEmail",
+  message: "What is the Manager's Email ?",
+ },
+ {
+  type: "input",
+  name: "managerOffice",
+  message: "What is the Manager's Office Number ?",
+ },
 ];
 
 //Menu Questions
 const menuQuestions = [
-  {
-    type: "list",
-    name: "menuAnswer",
-    message: "Choose what to do next:",
-    choices: ["build an Engineer", "build an Intern", "build an HTML file"],
-  },
+ {
+  type: "list",
+  name: "menuAnswer",
+  message: "Choose what to do next:",
+  choices: ["Build an Engineer", "Build an Intern", "Build an HTML file"],
+ },
 ];
 
 //Engineer Questions
 const engineerQuestions = [
-  {
-    type: "input",
-    name: "engineerName",
-    message: "What is the Engineer's Name ?",
-  },
+ {
+  type: "input",
+  name: "engineerName",
+  message: "What is the Engineer's Name ?",
+ },
+ {
+  type: "input",
+  name: "engineerId",
+  message: "What is the Engineer's Id ?",
+ },
+ {
+  type: "input",
+  name: "engineerEmail",
+  message: "What is the Engineer's Email ?",
+ },
+ {
+  type: "input",
+  name: "engineerGithub",
+  message: "What is the Engineer's Github ?",
+ },
+];
+
+//Intern Questions
+const internQuestions = [
+ {
+  type: "input",
+  name: "internName",
+  message: "What is the Intern's Name ?",
+ },
+ {
+  type: "input",
+  name: "internId",
+  message: "What is the Intern's Id ?",
+ },
+ {
+  type: "input",
+  name: "internEmail",
+  message: "What is the Intern's Email ?",
+ },
+ {
+  type: "input",
+  name: "internSchool",
+  message: "What is the Intern's School ?",
+ },
 ];
 
 //Functions
 function promptManagerFn() {
-  inquirer.prompt(managerQuestions).then((managerAnswer) => {
-    promptMenuFn();
-  });
+ inquirer.prompt(managerQuestions).then((managerAnswers) => {
+  const manager = new Manager(
+   managerAnswers.managerName,
+   managerAnswers.managerId,
+   managerAnswers.managerEmail,
+   managerAnswers.managerOffice
+  );
+  console.log(manager);
+  teamMembers.push(manager);
+  console.log(teamMembers);
+  promptMenuFn();
+ });
 }
 
 function promptMenuFn() {
-  inquirer.prompt(menuQuestions).then((menuAnswers) => {
-    if (menuAnswers.menuAnswer == "build an Engineer") {
-      promptEngineerFn();
-    }
-    if (menuAnswers.menuAnswer == "build an Intern") {
-      promptInternFn();
-    }
-    if (menuAnswers.menuAnswer == "build an HTML File") {
-      promptHtmlFn();
-    }
-  });
+ inquirer.prompt(menuQuestions).then((menuAnswers) => {
+  if (menuAnswers.menuAnswer == "Build an Engineer") {
+   promptEngineerFn();
+  }
+  if (menuAnswers.menuAnswer == "Build an Intern") {
+   promptInternFn();
+  }
+  if (menuAnswers.menuAnswer == "Build an HTML file") {
+   promptHtmlFn();
+  }
+ });
 }
 
 function promptEngineerFn() {
-  //console.log("buil an Engineer");
-  inquirer.prompt(engineerQuestions).then((engineerAnswer) => {
-    promptMenuFn();
-  });
-}
-function promptInternFn() {
-  //console.log("buil an Intern");
-}
-function promptHtmlFn() {
-  //console.log("buil an HTML file");
+ inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+  const engineer = new Engineer(
+   engineerAnswers.engineerName,
+   engineerAnswers.engineerId,
+   engineerAnswers.engineerEmail,
+   engineerAnswers.engineerGithub
+  );
+  console.log(engineer);
+  teamMembers.push(engineer);
+  console.log(teamMembers);
+  promptMenuFn();
+ });
 }
 
-//Prompts
-// promptManagerFn();
-const myNewEmployee = new Employee(
-  "Thiago",
-  "53",
-  "thiago.nasciutti@gmail.com"
-);
-myNewEmployee.sayHi();
+function promptInternFn() {
+ inquirer.prompt(internQuestions).then((internAnswers) => {
+  const intern = new Intern(
+   internAnswers.internName,
+   internAnswers.internId,
+   internAnswers.internEmail,
+   internAnswers.internSchool
+  );
+  console.log(intern);
+  teamMembers.push(intern);
+  console.log(teamMembers);
+  promptMenuFn();
+ });
+}
+
+function promptHtmlFn() {
+ console.log(teamMembers);
+ fs.writeFile("./dist/index.html", buildHtml(teamMembers), (error) => {
+  console.log(error);
+ });
+}
+
+//Call Functions
+promptManagerFn();
---
+++ src/template.js
@@ -0,0 +1,92 @@
+//Manager Card
+const managerCard = (manager) => {
+ return `
+    <div class="card mt-5" style="width: 18rem">
+        <div class="card-header text-center text-light bg-primary">
+            <h5 class="card-title">${manager.getName()}</h5>
+            <h5 class="card-title">${manager.getRole()}</h5>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group">
+                <li class="group-item">     ID:  ${manager.getId()}</li>
+                <li class="group-item">Email:<a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="group-item">Office Number:${manager.getOffice()}</li>
+            </ul>
+        </div>
+    </div>
+`;
+};
+
+//Engineer Card
+const engineerCard = (engineer) => {
+ return `
+    <div class="card mt-5" style="width: 18rem">
+        <div class="card-header text-center text-light bg-primary">
+            <h5 class="card-title">${engineer.getName()}</h5>
+            <h5 class="card-title">${engineer.getRole()}</h5>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group">
+                <li class="group-item">ID:${engineer.getId()}</li>
+                <li class="group-item">Email:<a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="group-item">Github:<a href="${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>
+    `;
+};
+
+//Intern Card
+const internCard = (intern) => {
+ return `
+    <div class="card mt-5" style="width: 18rem">
+        <div class="card-header  text-center text-light bg-primary">
+            <h5 class="card-title">${intern.getName()}</h5>
+            <h5 class="card-title">${intern.getRole()}</h5>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group">
+                <li class="group-item">ID:${intern.getId()}</li>
+                <li class="group-item">Email:<a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="group-item">School:${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+    `;
+};
+
+//Build HTML
+const buildHtml = (team) => {
+ return ` 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <title>Team Members</title>
+    </head>
+    <body>
+        <header>
+            <h1 class="p-3 text-center text-warning bg-dark">Your Team</h1>
+        </header>
+        <div class="d-flex flex-wrap justify-content-evenly align-items-evenly">
+            ${team.map((member) => {
+             if (member.getRole() === "Manager") {
+              return managerCard(member);
+             }
+             if (member.getRole() === "Engineer") {
+              return engineerCard(member);
+             }
+             if (member.getRole() === "Intern") {
+              return internCard(member);
+             }
+            })}
+        </div>
+    </body>
+    </html>
+    `;
+};
+
+module.exports = buildHtml;
